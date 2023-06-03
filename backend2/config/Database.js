eee2e4cd07a4e148653409f427f6ac1406dd4d5d
Daniel Febrian <danielfebrian61@gmail.com>
2023-06-03 12:49:12 +0700

    added root

--- backend2/config/Database.js
+++ backend2/config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('tcc','root','',{
+const db = new Sequelize('tcc','root','root',{
     host: '34.124.164.152',
     dialect: 'mysql'
 });
